2f84caf82dde07a9bfa2e363c58977621abb5dab
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-21 20:44:10 -0500

    chore: second commit.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1', /* Dependiendo del problema, cambias 3000 */
+  platformProviderCategoriesEndpointPath: '/categories', /* Esto es un ejemplo */
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+};
